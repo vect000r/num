452bf60f58bd304a3952012efd7357e284d9f888
vect0r <vectoreddd@gmail.com>
2024-10-17 20:36:33 +0200

    code finished

---
+++ num1/num1a.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+    return np.sin(x**3)
+
+def accurate_derivative(x):
+    return 3 * x**2 * np.cos(x**3)
+
+def approximated_derivative(f, x, h):
+    return (f(x + h) - f(x)) / h
+
+x = 0.2
+
+exact = accurate_derivative(x)
+
+h_values = np.logspace(-10, -1, num=10000)
+
+errors_float32 = []
+errors_float64 = []
+
+for h in h_values:
+    h_float32 = np.float32(h)
+    errors_float32.append(abs(approximated_derivative(f, np.float32(x), h_float32)-np.float32(exact)))
+
+for h in h_values:
+    h_float64 = np.float64(h)
+    errors_float64.append(abs(approximated_derivative(f, np.float64(x), h_float64) - np.float64(exact)))
+
+plt.figure(figsize=(10, 6))
+
+plt.loglog(h_values, errors_float32, label="Derivative approximation error float32", color='red')
+plt.loglog(h_values, errors_float64, label="Derivative approximation error float64", color='blue')
+
+plt.grid(True, "both", ls="--")
+plt.show()
